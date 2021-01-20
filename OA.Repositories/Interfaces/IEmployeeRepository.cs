09021432cdc31381d7baa455a0597e795b6fbb4d
honrado-the-bugfixer <the.real.el.honrado@gmail.com>
2021-01-20 11:34:06 +0100

    added Employee repository

---
+++ OA.Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using OA.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OA.Repositories.Interfaces
+{
+   public interface IEmployeeRepository : IRepository<Employee>
+    {
+        IEnumerable<Employee> GetEmployeesWithSupervisor();
+        Employee GetEmployeeWithSupervisor(int id);
+    }
+}
